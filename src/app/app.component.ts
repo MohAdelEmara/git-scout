39483a4b4e40f0e226a9b873b4f472c0f64a844f
Mohamed Emara <falacommohamed@gmail.com>
2025-04-09 18:30:56 -0300

    [FEAT-002]feat(footer): criação do footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavComponent } from "./components/nav/nav.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavComponent],
+  imports: [NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
